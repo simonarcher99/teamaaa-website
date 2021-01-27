c6c7fef0f715b0565cded9fa94fb163484a7440b
Simon Archer <simon.james.archer@gmail.com>
2021-01-27 23:06:49 +0000

    contact-form v1

--- src/teamaaa/static_my_proj/js/teamaaa.js
+++ src/teamaaa/static_my_proj/js/teamaaa.js
@@ -20,3 +20,13 @@ panels.forEach((panel) => {
     panel.addEventListener('mouseenter', () => panel.classList.add('active'))
     panel.addEventListener('mouseleave', () => panel.classList.remove('active'))
 })
+
+// Form animation
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    .join('')
+})
